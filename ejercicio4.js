426d52781589c4654f76543583219334611e37fb
mayerlyuribe <mayerlyuribe@colegioguanenta.edu.co>
2025-10-20 11:24:22 -0500

    finnn

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,18 +1,16 @@
-
 const frutas = {
-    PLATANO : 1.35,
-    MANZANA : 0.80,
-    PERA : 0.85,
-    NARANJA : 0.70
+    "PLATANO" : 1.35,
+    "MANZANA" : 0.80,
+    "PERA" : 0.85,
+    "NARANJA" : 0.70
 }
 
-let pedido = prompt("ingrese la fruta que desea comprar: ").toUpperCase;
+let pedido = prompt("ingrese la fruta que desea comprar: ").toUpperCase();
 let kilos = parseInt(prompt("cuantos kilos quiere?: "));
 
-array_frutas = Object.keys(frutas)
-
 if (frutas[pedido]){
-    console.log("si ta")
+    let precio = frutas[pedido] * kilos;
+    console.log(`el precio de ${kilos} kg de ${pedido.toLowerCase()} es de ${precio} pesos`);
+}else {
+    console.log(`la fruta no está disponible`)
 }
-
-console.log(frutas)
